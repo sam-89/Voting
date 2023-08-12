3d3b9b2c9226b2bb4580190a2c1e4277eac34595
Sumanta Kumar Patel <sumantakumar.patel@gmail.com>
2023-08-12 18:27:41 +0530

    update

--- voting.py
+++ voting.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# URL of the web page
 url = "https://siima.in/Voting/"
 
 # Number of iterations for the loop
